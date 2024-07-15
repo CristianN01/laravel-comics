6c28712d444d8959b78224a8ecf067a6de12a50a
CristianN01 <cristiannechifor71@gmail.com>
2024-07-15 18:55:12 +0200

    creta cartella img con tutte le foto dentro, aggiunto il header nella pagina

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,23 @@
 <header>
+    <section>
+        <div class="bg-color-page">
+            <div class="width-container">
+                <section class="logo">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="logo DC">
+                </section>
 
+                <section class="navbar">
+                    <nav>
+                        <ul>
+                            @foreach ($navComics as $link)
+                                <li class="{{$link['active']? 'active' : ''}}">
+                                    {{ $link['name'] }}
+                                </li>
+                            @endforeach
+                        </ul>
+                    </nav>
+                </section>
+            </div>
+        </div>
+    </section>
 </header>
